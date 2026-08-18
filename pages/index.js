44359cb869d319a16a893fa687223edc053d256f
Chelsea Jaena <cjbmangulabnan.student@ua.edu.ph>
2026-08-18 23:02:54 +0800

    Updated profile introduction

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,8 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi! I'm Chelsea, an IT student and aspiring developer based in web technologies.s</p>
+        <p>Hi! I’m Chelsea, an IT student and an aspiring developer specializing in web technologies. My hobbies include playing video games and badminton. My favorite songs are “Banana Pancakes” by Jack Johnson, “Better” by Zayn Malik, and some K-pop songs. My favorite food is anything with soup.
+           In five years, I see myself living in another country and enjoying life to the fullest.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
